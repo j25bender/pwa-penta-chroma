8461a0a0cbe934fa561471d5d94d93e44148629d
Jeff Bender <j25bender@gmail.com>
2018-03-16 10:47:42 -0600

    heroku updates

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ module.exports = {
       directory: './db/migrations'
     },
     useNullAsDefault: true
-  }
+  },
 
   // staging: {
   //   client: 'postgresql',
@@ -27,20 +27,13 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+  client: 'pg',
+  connection: process.env.DATABASE_URL + `?ssl=true`,
+  migrations: {
+    directory: './db/migrations'
+  },
+  useNullAsDefault: true
+  }
 
 };
