f926f55f4f772610f08bac41d38d7b56557065e6
Jeff Bender <j25bender@gmail.com>
2018-03-23 08:58:30 -0600

    moved service worker

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -121,7 +121,7 @@ const createColors = (palette) => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {    
-    navigator.serviceWorker.register('../../service-worker.js')
+    navigator.serviceWorker.register('../service-worker.js')
       .then(registration => {
         console.log('ServiceWorker registration successful');
       }).catch(error => {
